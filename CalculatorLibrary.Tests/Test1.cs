e84291277f7ab92c4881ab01c472fff3b0086f82
Ashwin8697 <ashwin869raj@gmail.com>
2026-05-13 21:48:41 +0530

    Add project files.

---
+++ CalculatorLibrary.Tests/Test1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorLibrary;
+
+namespace CalculatorLibrary.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private Calculator calc = new Calculator();
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(15, calc.Add(10, 5));
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(5, calc.Subtract(10, 5));
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            Assert.AreEqual(50, calc.Multiply(10, 5));
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            Assert.AreEqual(2, calc.Divide(10, 5));
+        }
+
+        [TestMethod]
+        public void DivideByZeroTest()
+        {
+            try
+            {
+                calc.Divide(10, 0);
+                Assert.Fail("Expected exception not thrown");
+            }
+            catch (DivideByZeroException)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
